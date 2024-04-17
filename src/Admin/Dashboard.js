764450237abc73385bea8720b5e1a2c6e04c6412
AnarchistHoneybun <thatonebipanda@gmail.com>
2024-04-18 01:07:01 +0530

    sync

--- src/Admin/Dashboard.js
+++ src/Admin/Dashboard.js
@@ -7,15 +7,16 @@ import LineChart from "./LineChart";
 function Dashboard() {
   const [totalUsers, setTotalUsers] = useState(0);
   const [totalBandwidth, setTotalBandwidth] = useState(0);
+  const [totalRequests, setTotalRequests] = useState(0);
 
   useEffect(() => {
     async function fetchTotalUsers() {
-      const { data, error } = await supabase.from("users").select();
+      const { data, count, error } = await supabase.from("users").select('*', { count: 'exact', head: true });
       console.log(data);
       if (error) {
         console.error('Error fetching total users:', error);
       } else {
-        setTotalUsers(data.length);
+        setTotalUsers(count);
       }
     }
     fetchTotalUsers();
@@ -34,12 +35,24 @@ function Dashboard() {
     fetchTotalBandwidth();
   }, []);
 
+  useEffect(() => {
+    async function fetchTotalRequests() {
+      const { data, count, error } = await supabase.from('api_call').select('call_id', { count: 'exact' });
+      if (error) {
+        console.error('Error fetching total requests:', error);
+      } else {
+        setTotalRequests(count);
+      }
+    }
+    fetchTotalRequests();
+  }, []);
+
   return (
       <div>
           <div className="ChipRow">
               <Chip color="#30475E" header="Total Users" value={totalUsers}/>
               <Chip color="#30475E" header="Total Bandwith" value={totalBandwidth}/>
-              <Chip color="#30475E" header="Total Requests"/>
+              <Chip color="#30475E" header="Total Requests" value={totalRequests}/>
           </div>
           <div className="ChartPlaceholder">
                 <LineChart/>
