2c8599d6a5f79571a8578bf917e3c2dbbb6a446f
mudit-51 <mudit.7.gupta+github@gmail.com>
2024-04-17 16:47:22 +0530

    created supabase client added two data fetches in key-requests and users

--- src/Admin/KeyRequests.js
+++ src/Admin/KeyRequests.js
@@ -1,4 +1,14 @@
+import { useEffect } from "react";
+import supabase from "../lib/supabase-client"
+
 function KeyRequests() {
+  useEffect(()=>{
+    async function getData(){
+      const data = await supabase.from("key_request").select();
+      console.log(data);
+    }
+    getData();
+  },[])
   return (
     <div>
       <h1>Key Requests</h1>
--- src/Admin/Users.js
+++ src/Admin/Users.js
@@ -1,4 +1,14 @@
+import supabase from "../lib/supabase-client";
+import { useEffect } from "react";
+
 function Users() {
+  useEffect(()=>{
+    async function getData(){
+      const data = await supabase.from("users").select();
+      console.log(data);
+    }
+    getData();
+  },[])
   return (
     <div>
       <h1>Users</h1>
