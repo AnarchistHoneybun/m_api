660fb1389b191eb1c82c6a1a81f34a6b9209d8b0
AnarchistHoneybun <thatonebipanda@gmail.com>
2024-04-18 09:27:05 +0530

    sync

--- src/Admin/BubbleChart.js
+++ src/Admin/BubbleChart.js
@@ -3,33 +3,64 @@ import { Bubble } from 'react-chartjs-2';
 import supabase from '../lib/supabase-client';
 
 function BubbleChart() {
-    const [chartData, setChartData] = useState({ datasets: [] });
+  const [chartData, setChartData] = useState({ datasets: [] });
 
+  // Create a color map for the users
+  const userColorMap = {};
 
   useEffect(() => {
     async function fetchData() {
       const { data, error } = await supabase.rpc("get_call_counts");
-        console.log(data);
+      console.log(data);
       if (error) {
         console.error('Error fetching data:', error);
       } else {
         setChartData({
-            datasets: data.map((item, index) => ({
-            label: `User ${item.username} on ${item.call_date}`,
+          datasets: data.map((item, index) => {
+            // If the user does not have a color yet, assign a random color
+            if (!userColorMap[item.username]) {
+              userColorMap[item.username] = `rgba(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255}, 0.6)`;
+            }
+
+            return {
+              label: item.username,
               data: [{
                 x: new Date(item.call_date).getTime(),
                 y: item.num_calls,
                 r: item.num_calls * 5, // Adjust the multiplier as needed
               }],
-              backgroundColor: `rgba(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255}, 0.6)`,
-            })),
-          });
-        }
+              backgroundColor: userColorMap[item.username],
+            };
+          }),
+        });
       }
-      fetchData();
-    }, []);
+    }
+    fetchData();
+  }, []);
 
-  return <Bubble data={chartData} />;
+  return (
+    <Bubble 
+      data={chartData} 
+      options={{
+        scales: {
+            y: {
+              display: false,
+            },
+            x: {
+                display: true,
+            }
+          },
+          plugins: {
+            legend: {
+              display: false,
+            },
+            tooltip: {
+              enabled: true,
+            },
+          },
+      }}
+    />
+  );
 }
 
 export default BubbleChart;
