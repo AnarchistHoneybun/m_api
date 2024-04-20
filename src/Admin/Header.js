2546c4758c7da972c3e0a5bb9454cec3f50ade2a
mudit-51 <mudit.7.gupta+github@gmail.com>
2024-04-20 14:21:05 +0530

    admin name hotfix

--- src/Admin/Header.js
+++ src/Admin/Header.js
@@ -3,7 +3,7 @@ import supabase from "../lib/supabase-client";
 
 // Header.js
 function Header() {
-    const [admin, setAdmin] = useState("Admin");
+    const [admin, setAdmin] = useState("");
 
   useEffect(()=>{
     async function getCurrentAdmin(){
@@ -17,7 +17,7 @@ function Header() {
   return (
       <div className="Header">
           <div className="Profile">
-              <p>Welcome, {admin}</p>
+              <p>{admin.length && `Welcome, ${admin}`}</p>
               <img
                   src="https://images.unsplash.com/photo-1713118774750-e1878e76b067?q=80&w=2844&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                   alt="Profile Icon" className="ProfileIcon"/>
