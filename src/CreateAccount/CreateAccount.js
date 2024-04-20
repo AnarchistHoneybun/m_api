1bda97b78bbee02a74436140341ce9e76102bfe4
AnarchistHoneybun <thatonebipanda@gmail.com>
2024-04-20 14:38:48 +0530

    removed password hashing

--- src/CreateAccount/CreateAccount.js
+++ src/CreateAccount/CreateAccount.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import "./CreateAccount.css";
 import supabase from "../lib/supabase-client";
-import {SHA256} from "crypto-js";
 
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/ReactToastify.css";
@@ -31,10 +30,9 @@ const CreateAccount = () => {
         if (res.error) {
           toast(res.error.message, { type: "error" });
         } else {
-          const hashedPassword = SHA256(password).toString();
           res = await supabase.from("users").insert({
             username: username,
-            password: hashedPassword,
+            password: password,
             email: email,
             id_type: idType,
             id_value: idValue,
